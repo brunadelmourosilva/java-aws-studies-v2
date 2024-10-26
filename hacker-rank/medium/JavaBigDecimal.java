e523695db9d653a00124a2a38c65febef49ae832
Bruna Delmouro <brunadelmouro@gmail.com>
2024-10-26 14:36:19 -0300

    Java BigDecimal - TBD

--- hacker-rank/medium/JavaBigDecimal.java
+++ hacker-rank/medium/JavaBigDecimal.java
@@ -1,6 +1,7 @@
 package medium;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class JavaBigDecimal {
@@ -11,21 +12,19 @@ public class JavaBigDecimal {
 
         int n = sc.nextInt();
 
-        String[] s = new String[n + 2]; // todo remove this '2'
+        String[] s = new String[n + 2];
 
         for (int i = 0; i < n; i++) {
             s[i] = sc.next();
         }
         sc.close();
 
-        //Write your code here // todo finish
+        //Write your code here
 
-        // convert string to bigdecimal
+        // convert string to bigdecimal and reorder list s
         for (int i = 0; i < n; i++) {
-            // todo maybe add null check due 2
 
-            for (int j = i + 1; j < n; j++) {
-                // todo maybe add null check due 2
+            for (int j = i + 1; j < n; j++) { // 0+1, 1+1, 2+1 ...
 
                 BigDecimal bdX = new BigDecimal(s[i]);
                 BigDecimal bdY = new BigDecimal(s[j]);
